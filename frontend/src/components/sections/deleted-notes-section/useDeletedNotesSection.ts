3c1318e373d0910dc4471d8c49e162f85f95c852
GeorgeNonis <Georgenonis@gmail.com>
2023-03-22 20:12:29 +0100

    Loading initial state on every section except lables

--- frontend/src/components/sections/deleted-notes-section/useDeletedNotesSection.ts
+++ frontend/src/components/sections/deleted-notes-section/useDeletedNotesSection.ts
@@ -1,11 +1,43 @@
 import { IRootState } from "../../../store/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getNotesHttp } from "../../../services";
+import { IfNetworkDown, isThereError } from "../../../utils";
+import { initial } from "../../../store/notes-slice";
+import {
+  errorState,
+  loadingInitialState as LoadInitialData,
+} from "../../../store/display-state-slice";
 
 export const useDeletedNotesSection = () => {
   const notesState = useSelector((state: IRootState) => state.notes);
+  const dispatch = useDispatch();
   const { error, loadingInitialState } = useSelector(
     (state: IRootState) => state.displayState
   );
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await getNotesHttp();
+      // console.log(response);
+
+      const sucessfullRequest = isThereError(response);
+      if (sucessfullRequest) {
+        dispatch(initial(response[0]));
+        // console.log("Sucessfull");
+      } else {
+        const msg = response[1]?.message!;
+        // console.log("UN-Sucessfull");
+        // console.log(msg);
+
+        dispatch(errorState(IfNetworkDown(msg)));
+      }
+
+      dispatch(LoadInitialData(false));
+    };
+    fetch();
+  }, []);
+
   const state = {
     notes: notesState,
     displayState: {
--- frontend/src/components/sections/pinned-notes-section/usePinnedNotesSection.ts
+++ frontend/src/components/sections/pinned-notes-section/usePinnedNotesSection.ts
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useDnd } from "../../../hooks/useDnD";
-import { sortNotes } from "../../../store/notes-slice";
+import { initial, sortNotes } from "../../../store/notes-slice";
 import { IRootState } from "../../../store/store";
 import { NoteObj } from "../../../interfaces/interfaces";
-import { DragEndUtil } from "../../../utils/utils";
-import { errorState } from "../../../store/display-state-slice";
+import { DragEndUtil, IfNetworkDown, isThereError } from "../../../utils/utils";
+import {
+  errorState,
+  loadingInitialState,
+} from "../../../store/display-state-slice";
+import { getNotesHttp } from "../../../services";
 
 export const usePinnedNotesSection = () => {
   const state = useSelector((state: IRootState) => state.notes);
@@ -26,6 +31,28 @@ export const usePinnedNotesSection = () => {
     };
     await DragEndUtil({ state, index, indexOf, cb, pinned: true });
   };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await getNotesHttp();
+      // console.log(response);
+
+      const sucessfullRequest = isThereError(response);
+      if (sucessfullRequest) {
+        dispatch(initial(response[0]));
+        // console.log("Sucessfull");
+      } else {
+        const msg = response[1]?.message!;
+        // console.log("UN-Sucessfull");
+        // console.log(msg);
+
+        dispatch(errorState(IfNetworkDown(msg)));
+      }
+
+      dispatch(loadingInitialState(false));
+    };
+    fetch();
+  }, []);
 
   return {
     onDragEnter,
