0d339cdcb748cc693132ca483d7d87392f636d11
GeorgeNonis <Georgenonis@gmail.com>
2023-03-22 12:00:29 +0100

    sd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+console.log();
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Note-Manager/",
