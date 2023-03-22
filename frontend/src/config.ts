6e4fc3022c2ca3cba62699c306f4425cea1e6a5c
GeorgeNonis <Georgenonis@gmail.com>
2023-03-22 12:06:11 +0100

    Added console

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,6 @@
 export const API_VERSION = import.meta.env.VITE_API_VERSION;
 export const BASE_URL = `${import.meta.env.VITE_BASE_URL}${API_VERSION}`;
-
+console.log("");
 export const BASE_UR1L = `http://localhost:8080/${API_VERSION}`;
 
 /**
