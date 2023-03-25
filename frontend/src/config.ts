ec4ccaa17509c88429067cd953bc2a6e72c8f104
GeorgeNonis <Georgenonis@gmail.com>
2023-03-25 12:16:02 +0100

    create env.production file

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,2 @@
 export const API_VERSION = import.meta.env.VITE_API_VERSION;
 export const BASE_URL = `${import.meta.env.VITE_BASE_URL}${API_VERSION}`;
-console.log("");
-export const BASE_UR1L = `http://localhost:8080/${API_VERSION}`;
