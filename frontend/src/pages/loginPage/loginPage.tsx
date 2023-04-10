2d453f49d042ef9848c449119d12f4d51a29a284
GeorgeNonis <Georgenonis@gmail.com>
2023-04-10 02:11:03 +0200

    Almost done with the login form. Need to add the warning P to show up when you fail on Regex.test and also to change from Sign up user to Login if you dont have acc

--- frontend/src/pages/loginPage/loginPage.tsx
+++ frontend/src/pages/loginPage/loginPage.tsx
@@ -1,17 +1,48 @@
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { TbCircleX, TbCircleCheck } from "react-icons/tb";
 import styles from "./styles.module.scss";
 import { useLoginPage } from "./useLoginPage";
 
 const LoginPage = () => {
   const { handlers, values } = useLoginPage();
-  const { confirmPasswordHover, passwordHover } = values;
-  const { setPasswordHover, setConfirmPasswordHover } = handlers;
+  const { confirmPasswordHover, passwordHover, showPassword } =
+    values.passwordValues;
+  const {
+    setPasswordHover,
+    setConfirmPasswordHover,
+    showPasswordHandler,
+    emailHandlers,
+    passwordHandlers,
+    confirmPasswordHandlers,
+  } = handlers;
+
   return (
     <main className={styles.main}>
       <form className={styles.form}>
         <fieldset>
-          <legend>E-Mail</legend>
+          <legend>
+            E-Mail:
+            <span
+              className={
+                values.emailValues.emailValid ? styles.show : styles.hide
+              }
+            >
+              <TbCircleCheck className={styles.correct} />
+            </span>
+            <span
+              className={
+                values.emailValues.email && !values.emailValues.emailValid
+                  ? styles.show
+                  : styles.hide
+              }
+            >
+              <TbCircleX className={styles.xmark} />
+            </span>
+          </legend>
           <input
+            onChange={(e) => emailHandlers.setEmail(e.target.value)}
+            onFocus={() => emailHandlers.setEmailFocus(true)}
+            onBlur={() => emailHandlers.setEmailFocus(false)}
             type="email"
             id="email"
             name="email"
@@ -19,26 +50,107 @@ const LoginPage = () => {
           />
         </fieldset>
         <fieldset>
-          <legend>Password</legend>
-          <input type="password" id="password" name="password" />
+          <legend>
+            Password:
+            <span
+              className={
+                values.passwordValues.passwordValid ? styles.show : styles.hide
+              }
+            >
+              <TbCircleCheck className={styles.correct} />
+            </span>
+            <span
+              className={
+                values.passwordValues.password &&
+                !values.passwordValues.passwordValid
+                  ? styles.show
+                  : styles.hide
+              }
+            >
+              <TbCircleX className={styles.xmark} />
+            </span>
+          </legend>
+          <input
+            onChange={(e) => passwordHandlers.setPassword(e.target.value)}
+            onFocus={() => passwordHandlers.setPasswordFocus(true)}
+            onBlur={() => passwordHandlers.setPasswordFocus(false)}
+            type={showPassword ? "password" : "text"}
+            id="password"
+            name="password"
+          />
           <span
-            onMouseEnter={() => setPasswordHover(true)}
-            onMouseLeave={() => setPasswordHover(false)}
+            onClick={showPasswordHandler}
+            onMouseEnter={() => setPasswordHover(!passwordHover)}
+            onMouseLeave={() => setPasswordHover(!passwordHover)}
           >
-            {passwordHover ? <AiFillEye /> : <AiFillEyeInvisible />}
+            {passwordHover || showPassword ? (
+              <AiFillEye />
+            ) : (
+              <AiFillEyeInvisible />
+            )}
           </span>
         </fieldset>
         <fieldset>
-          <legend>Confirm Password</legend>
-          <input type="password" id="password" name="password" />
+          <legend>
+            Confirm Password:
+            <span
+              className={
+                values.validMatch && values.passwordValues.passwordValid
+                  ? styles.show
+                  : styles.hide
+              }
+            >
+              <TbCircleCheck className={styles.correct} />
+            </span>
+            <span
+              className={
+                (values.confirmPasswordValues.confirmPassword &&
+                  !values.validMatch) ||
+                !values.passwordValues.passwordValid
+                  ? styles.show
+                  : styles.hide
+              }
+            >
+              <TbCircleX className={styles.xmark} />
+            </span>
+          </legend>
+          <input
+            onChange={(e) =>
+              confirmPasswordHandlers.setConfirmPassword(e.target.value)
+            }
+            onFocus={() =>
+              confirmPasswordHandlers.setConfirmPasswordFocus(true)
+            }
+            onBlur={() =>
+              confirmPasswordHandlers.setConfirmPasswordFocus(false)
+            }
+            type={showPassword ? "password" : "text"}
+            id="password"
+            name="password"
+          />
           <span
-            onMouseEnter={() => setConfirmPasswordHover(true)}
-            onMouseLeave={() => setConfirmPasswordHover(false)}
+            onClick={showPasswordHandler}
+            onMouseEnter={() => setConfirmPasswordHover(!confirmPasswordHover)}
+            onMouseLeave={() => setConfirmPasswordHover(!confirmPasswordHover)}
           >
-            {confirmPasswordHover ? <AiFillEye /> : <AiFillEyeInvisible />}
+            {confirmPasswordHover || showPassword ? (
+              <AiFillEye />
+            ) : (
+              <AiFillEyeInvisible />
+            )}
           </span>
         </fieldset>
-        <button className={styles.formloginbutton}>Login</button>
+        <button
+          className={styles.formloginbutton}
+          disabled={
+            !values.emailValues.emailValid ||
+            !values.passwordValues.passwordValid ||
+            !values.validMatch
+          }
+          style={{ cursor: !values.validInputs ? "not-allowed" : "pointer" }}
+        >
+          Login
+        </button>
       </form>
     </main>
   );
--- frontend/src/pages/loginPage/useLoginPage.ts
+++ frontend/src/pages/loginPage/useLoginPage.ts
@@ -1,7 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 
 export const useLoginPage = () => {
+  const PWD_REGEX: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,15}$/;
+  const USER_REGEX: RegExp =
+    /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
   const emailRef = useRef<HTMLInputElement>(null);
+  const errRef = useRef<HTMLParagraphElement>(null);
+
   const [email, setEmail] = useState("");
   const [emailFocus, setEmailFocus] = useState(false);
   const [emailValid, setEmailValid] = useState(false);
@@ -16,11 +22,54 @@ export const useLoginPage = () => {
 
   const [passwordHover, setPasswordHover] = useState(false);
   const [confirmPasswordHover, setConfirmPasswordHover] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [validInputs, setValidInputs] = useState(false);
+  const [validMatch, setValidMatch] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const showPasswordHandler = () => {
+    setShowPassword(!showPassword);
+    setPasswordHover(!passwordHover);
+    setConfirmPasswordHover(!confirmPasswordHover);
+  };
+
+  useEffect(() => {
+    emailRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    if (!emailFocus) return;
+    const result = USER_REGEX.test(email);
+    setEmailValid(result);
+  }, [email]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(password);
+    setPasswordValid(result);
+    const match = password === confirmPassword;
+    setValidMatch(match);
+
+    const validForm = emailValid && match;
+    setValidInputs(validForm);
+  }, [password, confirmPassword, email]);
+
+  const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const v1 = USER_REGEX.test(email);
+    const v2 = PWD_REGEX.test(password);
+    if (!v1 || !v2) {
+      errRef.current?.focus();
+      setErrorMsg("Invalid Entry");
+      return;
+    }
+  };
 
   const state = {
     values: {
-      passwordHover,
-      confirmPasswordHover,
+      validInputs,
+      errRef,
+      validMatch,
       emailValues: {
         email,
         emailFocus,
@@ -31,6 +80,9 @@ export const useLoginPage = () => {
         password,
         passwordFocus,
         passwordValid,
+        passwordHover,
+        confirmPasswordHover,
+        showPassword,
       },
       confirmPasswordValues: {
         confirmPassword,
@@ -41,12 +93,26 @@ export const useLoginPage = () => {
     handlers: {
       setPasswordHover,
       setConfirmPasswordHover,
+      setShowPassword,
+      showPasswordHandler,
+      setValidInputs,
+      emailHandlers: {
+        setEmail,
+        setEmailFocus,
+        setEmailValid,
+      },
+      passwordHandlers: {
+        setPasswordFocus,
+        setPassword,
+        setPasswordValid,
+      },
+      confirmPasswordHandlers: {
+        setConfirmPassword,
+        setConfirmPasswordFocus,
+        setConfirmPasswordValid,
+      },
     },
   };
-
-  useEffect(() => {
-    emailRef.current?.focus();
-  }, []);
 
   return { ...state };
 };
