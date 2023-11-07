35e116d7287415aec64c02667984e05c7c834dac
GeorgeNonis <georgenonis@gmail.com>
2023-11-07 19:24:08 +0100

    .

--- backend/routes/retrieveData.js
+++ backend/routes/retrieveData.js
@@ -2,7 +2,6 @@ import UserBluePrint from "../data/schema.js";
 
 const retrieveUser = async (req, res, next) => {
   try {
-    // Assuming req.user is the email of the authenticated user
     const userDocument = await UserBluePrint.findOne({
       email: req.user,
     }).exec();
@@ -11,10 +10,8 @@ const retrieveUser = async (req, res, next) => {
       return res.status(404).json({ message: "User not found." });
     }
 
-    // Attach the user document to the request object
     req.userDocument = userDocument;
 
-    // Call the next middleware in the stack
     next();
   } catch (error) {
     res.status(500).json({ message: "Internal server error", error });
